c0cf9b82b320f08cffa2d586295eb678db294e7e
jgarc172 <jgarc172@mail.ccsf.edu>
2019-11-02 19:02:21 -0700

    neighbors1 init

---
+++ neighbors1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace neighbors1
+{
+    class Program
+    {
+        static int[] Neighbors(int[] arr, int index)
+        {
+            
+
+            return arr;
+        }
+        static bool isIndex(int[] arr, int index)
+        {
+            return index >= 0 && index < arr.Length;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 1, 2, 4 };
+            Console.WriteLine( ToString(arr));
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int[] result = Neighbors(arr, 0);
+                Console.WriteLine("index {0} neighbors {1}", i, ToString(result));
+
+            }
+        }
+        static String ToString(int[] arr)
+        {
+            var result = "[ ";
+            foreach (var item in arr)
+            {
+                result =+ item + " ";
+            }
+            return result + "]";
+
+
+        }
+    }
+}
