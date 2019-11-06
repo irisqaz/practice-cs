1c0e863575d231dfd70f7b5d7ba4a02c542f5790
jgarc172 <jgarc172@mail.ccsf.edu>
2019-11-05 17:51:18 -0800

    used tuples

--- neighbors2/Program.cs
+++ neighbors2/Program.cs
@@ -5,11 +5,11 @@ namespace neighbors2
 {
     class Program
     {
-        static List<int>[] Neighbors(int[][] arr, int i, int j)
+        static List<Tuple<int, int>> Neighbors(int[][] arr, int i, int j)
         {
             // int[], int  -> int[]
             // neighbors are i and i-1 and i+1
-            var result = new List<List<int>>();
+            var result = new List<Tuple<int, int>>();
             int rLength = arr.Length;
             int cLength = 0;
             if (arr.Length > 0)
@@ -20,34 +20,30 @@ namespace neighbors2
             { // i
                 // neighbors are at offsets  -1 and + 1
                 var offsets = new int[] { -1, 1 };
-                List<int> pair;
-                // j is constant
-                foreach (var iOffSet in offsets)
-                {
-
-                    if (isIndex(rLength, i + iOffSet))
-                    {
-                        pair = new List<int>();
-                        pair.Add(i + iOffSet);
-                        pair.Add(j);
-                        result.Add(pair);
-                    }
-                }
+                Tuple<int, int> pair;
                 // i is constant
                 foreach (var jOffSet in offsets)
                 {
 
                     if (isIndex(cLength, j + jOffSet))
                     {
-                        pair = new List<int>();
-                        pair.Add(i);
-                        pair.Add(j + jOffSet);
+                        pair = Tuple.Create(i, j + jOffSet);
+                        result.Add(pair);
+                    }
+                }
+                // j is constant
+                foreach (var iOffSet in offsets)
+                {
+
+                    if (isIndex(rLength, i + iOffSet))
+                    {
+                        pair = Tuple.Create(i + iOffSet, j);
                         result.Add(pair);
                     }
                 }
             }
-            //Console.WriteLine(string.Join(",", result));
-            return result.ToArray();
+
+            return result;
         }
 
         static bool isIndex(int length, int index)
@@ -57,25 +53,26 @@ namespace neighbors2
         static void Main(string[] args)
         {
             int[][] arr = { new int[]{ 10, 22, 5, 15 },
-                            new int[]{ 33, 11, 9, 44}, 
-                            new int[]{ 22, 55, 3, 11} 
+                            new int[]{ 33, 11, 9, 44},
+                            new int[]{ 22, 55, 3, 11}
                           };
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.WriteLine(ToString(arr[i]));
             }
-            
+
             var rows = arr.Length;
             var columns = arr[0].Length;
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    List<int>[] result = Neighbors(arr, i, j);
+                    List<Tuple<int, int>> result = Neighbors(arr, i, j);
                     Console.WriteLine("index {0},{1} neighbors: {2}", i, j, ToString2(result));
                 }
 
             }
+            
             /*
             foreach (var i in new int[] { -1, arr.Length })
             {
@@ -104,12 +101,12 @@ namespace neighbors2
 
 
         }
-        static String ToString2(List<int>[] arr)
+        static String ToString2(List<Tuple<int, int>> pairs)
         {
             var result = "[ ";
-            foreach (var item in arr)
+            foreach (var pair in pairs)
             {
-                result += "(" + string.Join(",", item) + ") ";
+                result += pair + " ";
                 //Console.WriteLine(item);
             }
             return result + "]";
