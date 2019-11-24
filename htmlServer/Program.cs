a4954cfe05c2efa6414e95f866be69ce35fdd0fe
jgarc172 <jgarc172@mail.ccsf.edu>
2019-11-23 16:53:45 -0800

    html

---
+++ htmlServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace htmlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line = "";
+            while (line != "exit")
+            {
+                line = Console.ReadLine();
+                if (line.StartsWith("HTTP"))
+                {
+                    String reqBody = GetBody(line);
+                    String respBody = GetResponse(reqBody);
+                    String resp = String.Format("HTTP OK body:{0}", respBody);
+                    Console.WriteLine(resp);
+                }
+            }
+        }
+        static String GetBody(String str)
+        {
+            int start = str.IndexOf(':');
+            return str.Substring(start+1);
+        }
+        static String GetResponse(String body){
+            String resp = String.Format("<html><body>{0}</body></html>", body);
+            return resp;
+        }
+    }
+}
